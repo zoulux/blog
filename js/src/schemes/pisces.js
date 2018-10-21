3299bc7f24bd793b8e850770d05e344d26b7e49e
zou_luxian <zoulux@126.com>
2018-10-21 10:33:50 +0800

    Site updated: 2018-10-21 10:33:49

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,17 @@
-// build time:Sun Oct 21 2018 10:33:07 GMT+0800 (China Standard Time)
-$(document).ready(function(){var n=$(".header-inner"),e=$("#sidebar"),i=function(){return n.height()+CONFIG.sidebar.offset},t=function(n){return e.css({"margin-top":n})},r=window.matchMedia("(min-width: 991px)");t(i()).show(),r.addListener(function(n){n.matches&&t(i())})});
-//rebuild by neat 
+$(document).ready(function () {
+  var $headerInner = $('.header-inner');
+  var $sidebar = $('#sidebar');
+  var getSidebarTop = function(){
+    return $headerInner.height() + CONFIG.sidebar.offset;
+  };
+  var setSidebarMarginTop = function(sidebarTop){
+    return $sidebar.css({ 'margin-top': sidebarTop });
+  };
+  var mql = window.matchMedia('(min-width: 991px)');
+  setSidebarMarginTop(getSidebarTop()).show();
+  mql.addListener(function(e){
+    if(e.matches){
+      setSidebarMarginTop(getSidebarTop());
+    }
+  });
+});
