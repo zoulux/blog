20a3d90d7a22ae972cf46cdd16d1d125e0fc4d97
zou_luxian <zoulux@126.com>
2018-10-21 10:33:22 +0800

    Site updated: 2018-10-21 10:33:22

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Oct 21 2018 10:27:01 GMT+0800 (China Standard Time)
-$(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
+// build time:Sun Oct 21 2018 10:33:07 GMT+0800 (China Standard Time)
+$(document).ready(function(){var n=$(".header-inner"),e=$("#sidebar"),i=function(){return n.height()+CONFIG.sidebar.offset},t=function(n){return e.css({"margin-top":n})},r=window.matchMedia("(min-width: 991px)");t(i()).show(),r.addListener(function(n){n.matches&&t(i())})});
 //rebuild by neat 
